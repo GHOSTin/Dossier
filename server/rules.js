5c5032d09a3575305a2b8c4bf7159771f438b597
GHOSTin <lwhiteghostl@gmail.com>
2017-07-12 14:17:13 +0600

    Отключение модуля insecure

--- server/rules.js
+++ server/rules.js
@@ -4,5 +4,8 @@ Avatars.allow({
     'insert': function () {
         // add custom authentication code here
         return true;
+    },
+    download: function() {
+        return true;
     }
 });
