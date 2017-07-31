514abba166f5ce3b86d230c28e638ac0939f257f
GHOSTin <lwhiteghostl@gmail.com>
2017-07-31 15:46:34 +0600

    Диалог удаления абитуриентов

--- client/views/pages/appviews/students.js
+++ client/views/pages/appviews/students.js
@@ -1,4 +1,7 @@
 import {Students} from '/lib/collections/students'
+import swal from 'sweetalert2'
+
+import 'sweetalert2/dist/sweetalert2.min.css'
 
 Template.students.onCreated(function() {
     let self = this;
@@ -31,6 +34,30 @@ Template.students.events({
     'click .edit': ( event ) => {
         event.preventDefault();
         FlowRouter.go('/student/'+$(event.currentTarget).attr('data-user-id'))
+    },
+    'click .delete': ( event ) => {
+        event.preventDefault();
+        let id = $(event.currentTarget).data('user-id');
+        swal({
+            title: 'Вы уверены?',
+            text: 'Вы не сможете вернуть изменения.',
+            type: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#dd6b55',
+            confirmButtonClass: 'btn btn-primary',
+            cancelButtonClass: 'btn btn-default',
+            allowOutsideClick: false,
+            confirmButtonText: 'Да, удалить!',
+            cancelButtonText: 'Отменить'
+        }).then(() => {
+            Meteor.call('deleteStudent', id, function(error, response){
+                if(error){
+                    Bert.alert( error.reason, 'danger', 'fixed-top', 'fa-exclamation-triangle' );
+                    return false;
+                }
+                Bert.alert('Удаление прошло успешно.', 'success', 'fixed-top', 'fa-trash-o')
+            });
+        })
     },
     'keyup #userName': function(e, template) {
         let value = $('#userName').val().trim();
